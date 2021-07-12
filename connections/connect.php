15eb25c210b25741706b2ec8a4226b3f7084df94
capitofc <capitofernan101420@gmail.com>
2021-07-12 16:54:09 +0800

    Added php connection

---
+++ connections/connect.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+$conn = mysqli_connect($servername, $username, $password);
+
+if(!$conn){
+    die("connection failed : " . mysqli_connect_error());
+}else{
+    echo "Connected Succesfully";
+}
+?>
